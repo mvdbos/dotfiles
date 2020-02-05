57c81002bb001a9f70be39c22a5f9be14fbec24a
Matthijs van den Bos <matthijs@vandenbos.org>
2020-02-05 13:57:27 +0100

    Disable iterm2 shell integration

--- setup_osx.bash
+++ setup_osx.bash
@@ -10,3 +10,9 @@ brew install \
     bash bash-completion bat coreutils colordiff curl git git-extras htop the_silver_searcher stow tree vim wget
 brew cleanup -s
 brew analytics off
+
+echo "Setting iTerm preference file location"
+# Specify the preferences directory
+defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/.dotfiles/iterm2"
+# Tell iTerm2 to use the custom preferences in the directory
+defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
