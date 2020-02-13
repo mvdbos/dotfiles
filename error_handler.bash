bf0942659270de12fa9e404000c009f1f216ab9d
Matthijs van den Bos <matthijs@vandenbos.org>
2020-02-13 14:05:16 +0100

    Improve error handler

--- error_handler.bash
+++ error_handler.bash
@@ -4,5 +4,12 @@ set -e
 # keep track of the last executed command
 trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 # echo an error message before exiting
-trap 'exit_code=$?;echo -e "\n$(basename ${0}): \"${last_command}\" command failed with exit code ${exit_code}." >&2' EXIT
+function handle_error() {
+    exit_code=$?
+    if [[ $exit_code != 0 ]]; then
+        echo -e "\n$(basename ${0}): \"${last_command}\" command failed with exit code ${exit_code}." >&2
+    fi
+}
+trap handle_error EXIT
+
 
