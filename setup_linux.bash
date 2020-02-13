a2d5799f971e5afa60bd4c2e1e3a960269157b62
Matthijs van den Bos <matthijs@vandenbos.org>
2020-02-13 13:18:07 +0100

    Cleanup linux setup

--- setup_linux.bash
+++ setup_linux.bash
@@ -1,14 +1,13 @@
 echo "Installing packages..."
-sudo apt-get update
-sudo apt-get install -qy \
+sudo apt-get update -qq
+sudo apt-get install -qqy \
     bash bash-completion colordiff curl git git-extras htop markdown silversearcher-ag stow tree vim wget
-sudo apt autoremove
+sudo apt autoremove -qq
 
 echo "Setting timezone to Europe/Amsterdam"
 sudo timedatectl set-timezone Europe/Amsterdam
 
 # bat is not available as standard package in 18.04
-wget https://github.com/sharkdp/bat/releases/download/v0.12.1/bat_0.12.1_amd64.deb -O /tmp/bat.deb
-sudo dpkg -i /tmp/bat.deb
-
+wget -q https://github.com/sharkdp/bat/releases/download/v0.12.1/bat_0.12.1_amd64.deb -O /tmp/bat.deb
+sudo dpkg -i /tmp/bat.deb 1>/dev/null
 
