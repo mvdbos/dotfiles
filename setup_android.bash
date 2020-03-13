6804fb10131f24babf29f0c0022777bf10f42719
Matthijs van den Bos <matthijs@vandenbos.org>
2020-03-13 14:19:28 +0100

    Add coreutils to Android

--- setup_android.bash
+++ setup_android.bash
@@ -2,4 +2,4 @@ source error_handler.bash
 
 echo "Installing Apt packages..."
  pkg install \
-    bash bash-completion bat colordiff curl git htop silversearcher-ag stow tree vim wget
+    bash bash-completion bat colordiff coreutils curl git htop silversearcher-ag stow tree vim wget
