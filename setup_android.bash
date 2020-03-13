d82e38bf1be66d2dc4cca3b8048b1b1eef17fa12
Matthijs van den Bos <matthijs@vandenbos.org>
2020-03-13 14:22:20 +0100

    Make Android pkg install quiet

--- setup_android.bash
+++ setup_android.bash
@@ -1,5 +1,5 @@
 source error_handler.bash
 
 echo "Installing Apt packages..."
- pkg install \
+ pkg install -qqy \
     bash bash-completion bat colordiff coreutils curl git htop silversearcher-ag stow tree vim wget
