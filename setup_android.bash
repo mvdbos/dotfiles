3469a4229b29e717e0f219bdfc1065f7a2f47cf2
Matthijs van den Bos <matthijs@vandenbos.org>
2020-03-13 13:53:58 +0100

    Add bat as pkg for Android

--- setup_android.bash
+++ setup_android.bash
@@ -2,9 +2,4 @@ source error_handler.bash
 
 echo "Installing Apt packages..."
  pkg install \
-    bash bash-completion colordiff curl git git-extras htop markdown silversearcher-ag stow tree vim wget
-
-# bat is not available as standard package in 18.04
-wget -q https://github.com/sharkdp/bat/releases/download/v0.12.1/bat_0.12.1_amd64.deb -O $TMPDIR/bat.deb
-dpkg -i $TMPDIR/bat.deb 1>/dev/null
-
+    bash bash-completion bat colordiff curl git htop silversearcher-ag stow tree vim wget
