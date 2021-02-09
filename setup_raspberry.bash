1f474bb94d477138fdfacef2aed7713672f922b2
Matthijs van den Bos <matthijs@vandenbos.org>
2021-02-09 15:22:12 +0100

    Fix bat versions

--- setup_raspberry.bash
+++ setup_raspberry.bash
@@ -13,7 +13,7 @@ sudo snap install hub --classic
 echo "Setting timezone to Europe/Amsterdam"
 sudo timedatectl set-timezone Europe/Amsterdam
 
-# bat is not available as standard package in 18.04
-wget -q https://github.com/sharkdp/bat/releases/download/v0.12.1/bat_0.12.1_amd64.deb -O /tmp/bat.deb
+# bat is not available as standard package
+wget -q https://github.com/sharkdp/bat/releases/download/v0.17.1/bat_0.17.1_armhf.deb -O /tmp/bat.deb
 sudo dpkg -i /tmp/bat.deb 1>/dev/null
 
