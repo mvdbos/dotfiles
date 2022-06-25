23dccbeca8f91a3f2ef2ace19bb48d015349bd5b
Matthijs van den Bos <matthijs@vandenbos.org>
2022-06-25 19:44:18 +0200

    Add Java 8 install

--- setup_osx.bash
+++ setup_osx.bash
@@ -1,14 +1,19 @@
 source error_handler.bash
 
+# Install Homebrew
 if !which brew 2>/dev/null 1>/dev/null; then
     echo "Installing Homebrew..."
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
+# Install Homebrew bundle
 brew bundle install --no-upgrade --file=Brewfile
 
 # Install SDKMan
 curl -s "https://get.sdkman.io?rcupdate=false" | bash
+
+# Install Java 8
+sdk install java 8.0.332-zulu
 
 echo "Setting iTerm preference file location"
 # Specify the preferences directory
